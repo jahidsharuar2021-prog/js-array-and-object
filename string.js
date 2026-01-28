cebf3f59442bc983883a7ae36f4b91a1fb2e7601
jahidsharuar2021-prog <jahidsharuar2021@gmail.com>
2026-01-28 11:52:45 +0600

    js string and object

---
+++ string.js
@@ -0,0 +1,13 @@
+
+// string is immutable means not changeable
+let name='jahid';
+let number=[10,30,40,50,60];
+console.log(name);
+console.log(name.length)
+console.log(name[0]);
+name[0]='p'; //because its immutable datatype
+console.log(name);
+console.log(number.length);
+console.log(number);
+number[0]=100; //it's change because it's mutable datatype
+console.log(number);

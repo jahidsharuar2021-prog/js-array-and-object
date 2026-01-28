2d53e50a2ea4ad5db22f7cca2e227cccd04bb346
jahidsharuar2021-prog <jahidsharuar2021@gmail.com>
2026-01-28 15:57:44 +0600

    concat and includes and split and slice

--- slice.js
+++ slice.js
@@ -1,3 +1,24 @@
 const address="Netokuna";
 const sliceAddress=address.slice(2,5);
 console.log(sliceAddress);
+
+
+const sentence="I am good and hardworking person";
+console.log(sentence.split("a"));
+const name='rahim ,karim, lahim, jahim, fahim, dahim';
+const people=name.split(',');
+console.log(people);
+const friends=['rahim','rani','jani','pani','nani'];
+console.log(friends);
+console.log(friends.join('|'));
+console.log(friends.join('-'));
+
+
+// string concat korar
+
+const first='Jahid';
+const last='Hasan';
+const full_name=first+' '+last;
+console.log(full_name);
+console.log(first.concat('') .concat(last));
+console.log(last.includes('s'));

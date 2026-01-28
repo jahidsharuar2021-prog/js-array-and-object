7c3a6fbce1085e04304cb05bd98455cb69f583d2
jahidsharuar2021-prog <jahidsharuar2021@gmail.com>
2026-01-28 21:01:51 +0600

    reverse string

--- reverse.js
+++ reverse.js
@@ -1,9 +1,24 @@
 
 
 
-let reverse='';
-let sentence='my name is jahid hasan';
-for(const letter of sentence){
-    reverse=letter+reverse;
-}
-console.log(reverse);
+
+// for(const letter of sentence){
+//     console.log(letter);
+//     reverse=letter+reverse;
+// }
+// console.log(reverse);
+// let reverse='';
+// let sentence='my name is jahid hasan';
+// console.log(sentence.length);
+// for(let i=0;i<sentence.length;i++){
+//     console.log(i);
+//     console.log(sentence[i]);
+//     reverse=sentence[i]+reverse;
+// }
+// console.log(reverse);
+const sentence='my name is jahid hasan';
+const rev=sentence.split('').reverse().join('');
+console.log(rev);
+let name='jahid';
+const first=name.slice(2,4);
+console.log(first);

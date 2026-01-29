f242c95df29bbb327120108aa4c43dce1bc6e8f0
jahidsharuar2021-prog <jahidsharuar2021@gmail.com>
2026-01-29 09:00:29 +0600

    object

---
+++ object.js
@@ -0,0 +1,14 @@
+let student={
+    name:'jahid hasan',
+    dis:'Netrokuna',
+    id:1234,
+    arrtibute:'good boy',
+  'facitlites opiton':['free','dinner','free food','tutionfree']
+}
+console.log(student);
+console.log(student['name']);
+student.name='omit hasan';
+console.log(student['name']);
+console.log(student['facitlites_opiton']);
+student['facitlites_opiton']=['name','jahid','hasan'];
+console.log(student['facitlites_opiton']);
